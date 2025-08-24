61ef34e3b5ef064a753aa104d7e404e63644dfe0
Mustafa Ersin Şişman - WSL <sismanmustafaersin@gmail.com>
2025-08-24 16:56:08 +0300

    cpp00 ex00 bitti

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-using  namespace std;
-
-int main(void)
+int main(int ac, char **av)
 {
-    int sayı  = 5;
-
-    cout << sayı << endl;
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (int i = 1; i < ac; i++)
+    {
+        for (int j = 0; av[i][j]; j++)
+            std::cout << (char)toupper(av[i][j]);
+    }
+    std::cout << std::endl;
     return 0;
 }
