28acb82795b64eeec4558c72288e47fd1a5a0cdc
Mustafa Ersin Şişman <--global>
2025-09-08 16:07:43 +0300

    ex05 güncellendi

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -2,35 +2,35 @@
 
 void	Harl::debug(void)
 {
-	std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+	std::cout << GREEN << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << RESET << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << BLUE << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "WARINING: I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << YELLOW << "WARINING: I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << RESET << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << RED << "ERROR: This is unacceptable! I want to speak to the manager now." << RESET << std::endl;
 }
 
 void	Harl::complain(std::string level)
 {
 	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-	void (Harl::*funciton[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	void (Harl::*funcitons[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
 	for (int i = 0; i < 4; i++)
 	{
 		if (levels[i] == level)
 		{
-			(this->*funciton[i])();
+			(this->*funcitons[i])();
 			return;
 		}
 	}
