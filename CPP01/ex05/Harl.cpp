b3b772b792f654ab9449846ac898b0b516f2c822
Mustafa Ersin Şişman <--global>
2025-09-09 18:33:22 +0300

    ex05 mesajalr güncellendi

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -1,23 +1,27 @@
 #include "Harl.hpp"
 
-void	Harl::debug( void )
+void	Harl::debug(void)
 {
-	std::cout << G << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << W << std::endl;
+	std::cout	<< G << "[ DEBUG ]" << std::endl
+			<< W << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
-void	Harl::info( void )
+void	Harl::info(void)
 {
-	std::cout << B << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << W << std::endl;
+	std::cout	<< B << "[ INFO ]" << std::endl
+			<< W << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void	Harl::warning( void )
+void	Harl::warning(void)
 {
-	std::cout << Y << "WARINING: I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << W << std::endl;
+	std::cout	<< Y << "[ WARINING ]" << std::endl
+			<< W << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
-void	Harl::error( void )
+void	Harl::error(void)
 {
-	std::cout << R << "ERROR: This is unacceptable! I want to speak to the manager now." << W << std::endl;
+	std::cout	<< R << "[ ERROR ] "<< std::endl
+			<< W << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void	Harl::complain(std::string level)
