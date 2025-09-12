873bb1f37a84e73bad2bc9f597dd2989cded7a3f
Mustafa Ersin Şişman <--global>
2025-09-12 17:38:16 +0300

    CPP02 başlandı

---
+++ CPP02/ex00/Fixed.cpp
@@ -0,0 +1,42 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( const int raw ) : raw(raw), bits(8)
+{
+	std::cout	<< Y << "Default constructor called"
+			<< W << std::endl;	
+}
+
+Fixed::Fixed( const Fixed& copy ) : raw(copy.raw), bits(copy.bits)
+{
+	std::cout	<< Y << "Copy constructor called"
+			<< W << std::endl;
+}
+
+Fixed&	Fixed::operator=( const Fixed& copy )
+{
+	std::cout	<< G << "Copy assignment operator called"
+			<< W << std::endl;
+
+	if (this != &copy)
+	{
+		raw = copy.raw;
+		bits = copy.bits;
+	}
+
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout	<< R << "Destructor called"
+			<< W << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout	<< B << "getRawBits member function called"
+			<< W << std::endl;
+	return this->raw;
+}
+
+void Fixed::setRawBits( int const raw ) {this->raw = raw;}
---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+# define R	"\033[1;31m"
+# define G	"\033[1;32m"
+# define Y	"\033[1;33m"
+# define B	"\033[1;34m"
+# define W	"\033[0m"
+
+class Fixed
+{
+	public:
+		Fixed( const int raw );
+		Fixed( const Fixed& copy );
+		Fixed&	operator=( const Fixed& copy );
+		~Fixed();
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+	private:
+		int			raw;
+		static const int	bits;
+};
+
+#endif
